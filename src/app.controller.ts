308fe93dcee81d1b5246f3098397bf05a4691100
Fareez Ahamed <fareez.ahamed@gmail.com>
2023-11-13 20:40:24 +0530

    Committed original code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService, LocalAuthGuard, BasicAuthGuard } from './auth';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(['', 'ping'])
+  healthCheck(): any {
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+    };
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('api/auth/login')
+  async login(@Req() req) {
+    const token = this.authService.login(req.user, 'basic');
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: {
+        ...token,
+      },
+    };
+  }
+
+  @UseGuards(BasicAuthGuard)
+  @Get('api/profile')
+  async getProfile(@Req() req) {
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+      data: {
+        user: req.user,
+      },
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { OrderModule } from './order/order.module';
+import { CartModule } from './cart/cart.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CartModule, OrderModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
